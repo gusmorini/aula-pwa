442664dfc72253d9a4b2283cb48d125153f56165
gustavo morini <gustavommh@gmail.com>
2018-10-28 13:29:46 -0300

    home

--- js/home.js
+++ js/home.js
@@ -1,36 +1,34 @@
 $(document).ready(function(){
+	//produtos do cache
+	produtos = localStorage.getItem("produtos");
 
-	// produtos do cache
-	produtos = localStorage.getItem("produtos")
-
-	// produtos do JSON
-	$.getJSON('json/produto.php', function(){
-		$(".produto").html(`<img src='imagens/load.gif'> Carregando produtos`)
+	$.getJSON("json/produto.php", function(){
+		$(".produto").html("<img src='imagens/load.gif'> Carregando produtos");
 	}).done(function(dados){
-		console.log('carregando produtos do JSON')
-		preencherProdutos(dados)
-		cache = JSON.stringify(dados)
-		localStorage.setItem('produtos', dados)
+		console.log("Carregando produtos do JSON");
+		//jogar os dados no .produto
+		preencherProdutos(dados);
+		cache = JSON.stringify(dados);
+		localStorage.setItem("produtos",dados);
 	}).fail(function(){
-		console.log('carregando produtos do cache')
-		dados = JSON.parse(produtos)
-		preencherProdutos(produtos)
+		console.log("Carregando produtos do Cache");
+		dados = JSON.parse(produtos);
+		preencherProdutos(dados);
 	})
 
-	//função preencher produtos
-	function preencherProdutos(dados){
-		$.each(dados, function (key, val){
-
-			$('.produto').html('')
-			$('.produto').append(`	<div class='col 13 m6 s12 center-align'>
-										<div class='card'>
-											<img src='${val.foto}' class='responsive-img'>
-											<p>${val.nome}</p>
-											<p class='valor'>R$ ${val.valor}</p>
-											<a href='produto/${val.id}' class='btn red darken-4'>detalhes</a>
-										</div>
-									</div>`)
-		})
+	//funcao para preencher os produtos
+	function preencherProdutos(dados) {
+		$(".produto").html("");
+		$.each(dados, function (key,val) {
+			$(".produto").append(`
+				<div class='col l3 m6 s12 center-align'>
+					<div class='card'>
+					<img src='${val.foto}' class='responsive-img'>
+					<p>${val.nome}</p>
+					<p class='valor'>R$ ${val.valor}</p>
+					<a href='produto/${val.id}' class='btn red darken-4'>Detalhes</a>
+					</div>
+				</div>`);
+		});
 	}
-
 })
